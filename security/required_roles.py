825db81ff6d8060a9353f5daab77fae74f48d1ed
Matheus Novo <matheusnovo28@gmail.com>
2024-11-16 03:49:21 -0300

    Corrige o erro de verificação e ajusta o retorno para o padrão.

--- security/required_roles.py
+++ security/required_roles.py
@@ -6,14 +6,14 @@ from functools import wraps
 user_controller = UserController()
 
 
-def required_rules(rule_list):
+def required_roles(role_list):
     def decorator(f):
         @wraps(f)
         def wrapped(*args, **kwargs):
             current_user = user_controller.get_user(get_jwt_identity())
-            user_rule = current_user['rule']
-            if user_rule not in rule_list:
-                return 'Unauthorized', 401
+            user_role = current_user['role']
+            if user_role not in role_list:
+                return {"error": "Acesso não autorizado"}, 401
             return f(*args, **kwargs)
         return wrapped
     return decorator
